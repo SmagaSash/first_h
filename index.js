94e3502c6c181ee0e6fc4203c1c9d050adc77f92
Smaga Sasha <Smaga.sasha@gmail.com>
2022-09-17 18:06:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var buttonPlus = document.getElementById('buttonPlus');
+var buttonMinus = document.getElementById('buttonMinus');
+var buttonMultipay = document.getElementById('buttonMultipay');
+var buttonDivide = document.getElementById('buttonDivide');
+
+function onButtonPlusClick() {
+    console.log('onButtonPlusClick');
+}
+
+function onButtonMinusClick() {
+    console.log('onbuttonMinusClick');
+}
+
+function onButtonMultipayClick() {
+    console.log('onButtonMultipayClick');
+}
+
+function onButtonDivideClick() {
+    console.log('onButtonDivideClick');
+}
+
+onButtonDivideClick()
